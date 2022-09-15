ed06a4674f49cad1c9b3d9f3f7aa6e9f246842cc
Кирилл Полосов <kirillpolosov@MacBook-Pro-Kirill.local>
2022-09-15 22:17:26 +0300

    MOVIE-1: create project

---
+++ Movie/Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Кирилл Полосов on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
